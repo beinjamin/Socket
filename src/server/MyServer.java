08e01f3f9253aec065d980831d1354d53a8d3e5d
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-02-12 19:44:53 +0100

    Attende de connexion en faissant appel accept

--- src/server/MyServer.java
+++ src/server/MyServer.java
@@ -1,11 +1,15 @@
 package server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MyServer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ServerSocket ss=new ServerSocket(1234);
+		System.out.print("J attend une connexion...");
+		Socket s = ss.accept();
 
 	}
 
