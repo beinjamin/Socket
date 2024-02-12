702669f71073294038eb68e90b7b6a0e4163b691
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-02-12 20:14:09 +0100

    Mise en place cote client

--- src/client/MyClient.java
+++ src/client/MyClient.java
@@ -1,9 +1,25 @@
 package client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class MyClient {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		System.out.print("Je me connecte ou serveur ");
+		Socket socket=new Socket("localhost",1234);
+		InputStream  is=socket.getInputStream();
+		OutputStream os=socket.getOutputStream();
+		
+		/*
+		 * Saisir les nombre ou clavier
+		 */
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Donner un nombre");
 
 	}
 
