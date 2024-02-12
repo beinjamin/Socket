6b9d9460df3121d27f0fee34186e75232fa4388f
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-02-12 19:50:43 +0100

    Finalisation de la creation du serveur

--- src/server/MyServer.java
+++ src/server/MyServer.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,7 +12,19 @@ public class MyServer {
 		ServerSocket ss=new ServerSocket(1234);
 		System.out.print("J attend une connexion...");
 		Socket s = ss.accept();
+		
+		InputStream is=s.getInputStream();
+		OutputStream os = s.getOutputStream();
+		
+		System.out.print("J attend que le client envoi un octet");
+		int nb=is.read();
+		System.out.print("J ai recu un nombre " +nb);
 
+		int res=nb*5;
+		System.out.print("J envoi la reponse" +res);
+
+		os.write(res);
+		s.close();
 	}
 
 }
