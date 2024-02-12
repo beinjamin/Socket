0c44062b7883b683ebad087e9ac0cdeb4ea7c6af
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-02-12 21:17:44 +0100

    Creation boucle pour connect le client a n importe quelle moment

--- src/server/ServeurMT.java
+++ src/server/ServeurMT.java
@@ -1,5 +1,9 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServeurMT extends Thread {
 
 	public static void main(String[] args) {
@@ -9,6 +13,16 @@ public class ServeurMT extends Thread {
 	}
 	
 	public void run() {
+		try {
+			ServerSocket ss =new ServerSocket(1234);
+			while(true) {
+				Socket socket = ss.accept();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
